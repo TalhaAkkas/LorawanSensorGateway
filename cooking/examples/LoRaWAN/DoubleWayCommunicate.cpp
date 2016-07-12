7b639b342e28330e105475b43a7aa2155403e565
M. Talha Buyukakkaslar <m.talhabuyukakkaslar@Ms-MacBook-Pro.local>
2016-07-12 20:07:29 +0300

    no message

--- cooking/examples/LoRaWAN/DoubleWayCommunicate.cpp
+++ cooking/examples/LoRaWAN/DoubleWayCommunicate.cpp
@@ -58,7 +58,7 @@ uint8_t sock = SOCKET0;
 //////////////////////////////////////////////
 uint8_t power = 15;
 uint32_t frequency = 868100000;
-char spreading_factor[] = "sf7";
+char spreading_factor[] = "sf12";
 char coding_rate[] = "4/5";
 uint16_t bandwidth = 500;
 char crc_mode[] = "on";
