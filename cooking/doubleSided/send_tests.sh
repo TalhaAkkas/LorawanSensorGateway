28063835f8f20a8b9faf8a1164932f0963c735eb
M. Talha Buyukakkaslar <m.talhabuyukakkaslar@Ms-MacBook-Pro.local>
2016-07-26 16:36:19 +0300

    remove delay diff

--- cooking/doubleSided/send_tests.sh
+++ cooking/doubleSided/send_tests.sh
@@ -3,7 +3,7 @@
 power_levels=(15 14 11 8 5 2)
 sfs=(sf12 sf11 sf10 sf9 sf8 sf7)
 bws=(125 250 500)
-delays=(100 150 200 250)
+delays=(150)
 for pw in ${power_levels[*]}; do
 	for sf in ${sfs[*]}; do
 		for bw in ${bws[*]}; do
