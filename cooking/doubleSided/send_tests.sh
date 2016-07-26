593805b9bea725d08f7c2e6a6bb4a43b4d984932
M. Talha Buyukakkaslar <m.talhabuyukakkaslar@Ms-MacBook-Pro.local>
2016-07-26 17:37:49 +0300

    remove delays

--- cooking/doubleSided/send_tests.sh
+++ cooking/doubleSided/send_tests.sh
@@ -3,14 +3,11 @@
 power_levels=(15 14 11 8 5 2)
 sfs=(sf12 sf11 sf10 sf9 sf8 sf7)
 bws=(125 250 500)
-delays=(150)
 for pw in ${power_levels[*]}; do
 	for sf in ${sfs[*]}; do
 		for bw in ${bws[*]}; do
-			for del in ${delays[*]}; do
-				sudo ./send_packets.cpp_exe "$pw" 868100000 "$sf" 4/5 "$bw" on "$del" "$1"
-				sleep 1
-			done
+			sudo ./send_packets.cpp_exe "$pw" 868100000 "$sf" 4/5 "$bw" on 150 "$1"
+			sleep 1
 		done
 	done
 done
