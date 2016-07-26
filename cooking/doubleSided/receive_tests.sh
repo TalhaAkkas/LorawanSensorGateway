d5a89b91817911683fb94cc12c03a5555aa63d59
M. Talha Buyukakkaslar <m.talhabuyukakkaslar@Ms-MacBook-Pro.local>
2016-07-26 15:53:29 +0300

    tests

---
+++ cooking/doubleSided/receive_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+power_levels=(15 14 11 8 5 2)
+sfs=(sf12 sf11 sf10 sf9 sf8 sf7)
+bws=(125 250 500)
+delays=(100 150 200 250)
+for pw in ${power_levels[*]}; do
+	for sf in ${sfs[*]}; do
+		for bw in ${bws[*]}; do
+			for del in ${delays[*]}; do
+				./receive_packets.cpp_exe $pw 868100000 $sf 4/5 $bw on del $1
+			done
+		done
+	done
+done
+#sudo ./receive_packets.cpp_exe 15 868100000 sf12 4/5 125 on 1000 1
